f8cb1412aec5fed453d988f8059f87aee4d4cd27
Amaan Khan <amaan7581@gmail.com>
2023-12-26 15:15:02 +0530

    modified configs

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,8 +6,9 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
+    # app.config.from_pyfile("config.py", silent=True)
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "ivegotnewrulesicountem")
+    app.config.update(
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
